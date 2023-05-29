46123c644da251e9d4dfc6746efb538b7dadeb79
Fabio Vieira <fabinvieira@gmail.com>
2023-05-29 12:32:06 -0300

    Primeiro commit

---
+++ FormacaoDotNetDeveloper/EstacionamentoApp/EstacionamentoApp/Model/Veiculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoApp.Model
+{
+    public class Veiculo
+    {
+        public Guid Id { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }        
+        
+        private static List<Veiculo> Veiculos = new List<Veiculo>();
+
+        public Veiculo(string placa)
+        {
+            this.Placa = placa;
+        }
+
+        public static void AdicionarVeiculo(string placa, string modelo)
+        {
+            Veiculo veiculo = new Veiculo(placa);
+            
+            veiculo.Id = Guid.NewGuid();
+            veiculo.Modelo = modelo;
+            
+            Veiculos.Add(veiculo);            
+            
+        }
+
+        public static List<Veiculo> ListarVeiculos()
+        {
+            List<Veiculo> veiculos = new List<Veiculo>();
+            foreach (var item in Veiculos)
+            {
+                veiculos.Add(item);
+            }
+            return veiculos;
+        }
+    }
+}
---
+++ FormacaoDotNetDeveloper/EstacionamentoApp/EstacionamentoApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using EstacionamentoApp.Model;
+
+bool sair=false;
+
+while(sair == false)
+{
+    Console.Write("Informe a placa do veículo: ");
+    var placa = Console.ReadLine();    
+    Console.Write("Informe o modelo: ");
+    var modelo = Console.ReadLine();
+    Veiculo.AdicionarVeiculo(placa, modelo);
+    var veiculos = Veiculo.ListarVeiculos();
+    foreach (var item in veiculos)
+    {
+        Console.WriteLine($"Placa:{item.Placa}");
+        Console.WriteLine($"Placa:{item.Modelo}");
+    }
+   
+    Console.Write("Deseja cadastrar outro veículo?(S/N)");
+    var resposta = Console.ReadLine();
+    if(resposta.ToUpper() == "N")
+        sair = true;
+}
+
