d75388b1f4c82bd8cb9716a9946c14d2c3ee29d4
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2024-01-11 17:36:16 -0300

    first committ credit-application-system

---
+++ Bootcamp-Kotlin/credit-application-system/src/main/kotlin/com/fabioluizvieirasilva/credit/application/system/CreditApplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.fabioluizvieirasilva.credit.application.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationSystemApplication>(*args)
+}
