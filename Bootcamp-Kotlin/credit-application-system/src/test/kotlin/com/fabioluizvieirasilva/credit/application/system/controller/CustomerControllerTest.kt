b217170b7ada5539eca18e5bc71fea90b3f84fe2
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2024-02-06 22:18:24 -0300

    feature(testes):Finalizando os testes integrados de CustomerController

--- Bootcamp-Kotlin/credit-application-system/src/test/kotlin/com/fabioluizvieirasilva/credit/application/system/controller/CustomerControllerTest.kt
+++ Bootcamp-Kotlin/credit-application-system/src/test/kotlin/com/fabioluizvieirasilva/credit/application/system/controller/CustomerControllerTest.kt
@@ -1,6 +1,7 @@
 package com.fabioluizvieirasilva.credit.application.system.controller
 
 import com.fabioluizvieirasilva.credit.application.system.dto.CustomerDto
+import com.fabioluizvieirasilva.credit.application.system.dto.CustomerUpdateDto
 import com.fabioluizvieirasilva.credit.application.system.entity.Address
 import com.fabioluizvieirasilva.credit.application.system.entity.Customer
 import com.fabioluizvieirasilva.credit.application.system.repository.CustomerRepository
@@ -51,6 +52,125 @@ class CustomerControllerTest {
             .andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should not save a customer with same CPF and return 409 status`(){
+        //given
+        customerRepository.save(builderCustomerDto().toEntity())
+        val customerDto: CustomerDto = builderCustomerDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isConflict)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("409"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("class org.springframework.dao.DataIntegrityViolationException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+
+    }
+
+    @Test
+    fun `should not save a customer with firstName empty and return 400 status`(){
+        val customerDto: CustomerDto = builderCustomerDto(firstName = "")
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("class org.springframework.web.bind.MethodArgumentNotValidException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
+    @Test
+    fun `should find customer by id and return 200 status`(){
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
+
+        mockMvc.perform(MockMvcRequestBuilders.get("$URL/${customer.id}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Fabio Luiz"))
+    }
+
+    @Test
+    fun `should not find customer by id and return 400 status`(){
+        val invalidId: Long = 2L
+
+        mockMvc.perform(MockMvcRequestBuilders.get("$URL/${invalidId}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("class com.fabioluizvieirasilva.credit.application.system.exception.BusinessException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
+    @Test
+    fun `should delete customer by id and return 204 status`(){
+        //given
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("$URL/${customer.id}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isNoContent)
+    }
+
+    @Test
+    fun `should not delete customer by id and return 400 status`(){
+        //given
+        val invalidId: Long = 2L
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("$URL/${invalidId}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("class com.fabioluizvieirasilva.credit.application.system.exception.BusinessException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
+    @Test
+    fun `should updated customer and return 200 status`(){
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpDateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("$URL?customerId=${customer.id}")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Fabio Luiz update"))
+
+    }
+
+    @Test
+    fun `should not updated customer and return 400 status`(){
+        val invalidId: Long = 2L
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpDateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("$URL?customerId=${invalidId}")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("class com.fabioluizvieirasilva.credit.application.system.exception.BusinessException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
     private fun builderCustomerDto(
         firstName: String = "Fabio Luiz",
         lastName: String = "Vieira da Silva",
@@ -67,6 +187,20 @@ class CustomerControllerTest {
         cpf = cpf,
         email = email,
         password = password,
+        zipCode = zipCode,
+        street = street,
+        income = income
+    )
+
+    private fun builderCustomerUpDateDto(
+        firstName: String = "Fabio Luiz update",
+        lastName: String = "Vieira da Silva update",
+        zipCode: String="23456879",
+        street: String="Rua do Fabio, n147 update",
+        income: BigDecimal = BigDecimal.valueOf(5000.0)
+    )= CustomerUpdateDto(
+        firstName = firstName,
+        lastName = lastName,
         zipCode = zipCode,
         street = street,
         income = income
